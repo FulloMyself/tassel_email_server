d82a377804658cdbcca154f1b1baa0941207eec0
FullofMyself <wmaseko1@live.com>
2025-10-09 12:54:35 +0200

    Tassel Voucher Update

---
+++ vouchers.js
@@ -0,0 +1,17 @@
+// vouchers.js
+exports.vouchers = [
+  {
+    code: "TASSEL10",
+    type: "percent",
+    value: 10,
+    description: "10% off your order",
+    active: true,
+  },
+  {
+    code: "SPRING50",
+    type: "fixed",
+    value: 50,
+    description: "R50 off your total order",
+    active: true,
+  },
+];
