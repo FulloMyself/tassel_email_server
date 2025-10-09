aeed636ecde82f853e6eedd9085333c7d0bb7db4
FullofMyself <wmaseko1@live.com>
2025-10-09 21:46:12 +0200

    Tassel Delivery Update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: process.env.SMTP_SECURE === "true",
+  secure: process.env.SMTP_SECURE === "false",
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
